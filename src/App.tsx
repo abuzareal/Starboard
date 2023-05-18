4510e318bd5f0fa56b9330746d2bdad3c3e7350d
nischalmudennavar <nischalmudennavar@gmail.com>
2023-05-18 23:16:42 +0530

    added stuff

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,12 @@ import Notes from "./components/UI/Notes/Notes";
 import { app, p, h1 } from "./styles/App.css";
 import "./styles/vars.css";
 import { darkTheme, lightTheme } from "./styles/vars.css";
+// lightTheme 
 
 function App() {
   return (
     <div>
-      <div className={`${darkTheme} ${app}`}>
+      <div className={`${lightTheme} ${app}`}>
         <h1 className={h1}>👾 Starboard 👾</h1>
         <Card>
           <h3>Dark Theme</h3>
