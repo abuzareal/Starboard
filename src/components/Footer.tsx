a0c288704101a25e6cfc50387f30a9f9a652eb8f
Abuzar <abuzart1999@gmail.com>
2023-05-22 17:34:01 +0530

    Sanitized Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,9 @@
 import React from "react";
-import { DiReact } from "react-icons/di";
 import { cardStyle } from "./UI/Cards/Card.css";
-import { footer } from "./Footer.css";
 
 function Footer() {
   return (
-    <div className={`${cardStyle.glass} +${footer} `}>
+    <div className={cardStyle.glass}>
       <p>Made with ⚛️ by Nischal and Abuzar</p>
     </div>
   );
