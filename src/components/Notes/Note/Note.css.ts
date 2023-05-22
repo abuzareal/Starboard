3c8701c3a8b624baf56d66c2cfaa9b6da5a130de
Abuzar <abuzart1999@gmail.com>
2023-05-22 13:17:44 +0530

    Pulsating animation

--- src/components/Notes/Note/Note.css.ts
+++ src/components/Notes/Note/Note.css.ts
@@ -1,5 +1,25 @@
-import { style } from "@vanilla-extract/css";
+import { style, keyframes } from "@vanilla-extract/css";
 import { vars } from "../../../styles/vars.css";
+
+const pulsate = keyframes({
+  "0%": {
+    boxShadow: "0px 0px 0px 0px #8f43ee00",
+  },
+  "50%": {
+    boxShadow: "0px 0px 100px 5px #8f43ee",
+  },
+  "100%": {
+    boxShadow: "0px 0px 0px 0px #8f43ee00",
+  },
+});
+
+export const animation = style({
+  animation: `${pulsate} 1.3s ease infinite`,
+  height: "auto",
+  maxWidth: "30%",
+  border: `${vars.effects.border} !important`,
+  position: "sticky",
+});
 
 export const noteStyle = style({
   height: "auto",
--- src/components/Notes/Note/Note.tsx
+++ src/components/Notes/Note/Note.tsx
@@ -1,16 +1,30 @@
+import { useState } from "react";
 import Card from "../../UI/Cards/Card";
-import { noteStyle } from "./Note.css";
+import { animation, noteStyle } from "./Note.css";
 import Draggable from "react-draggable";
 
 const Note = (props: any) => {
+  const [isDragged, setIsDragged] = useState(false);
   return (
-    <Draggable defaultPosition={{ x: 0, y: 0 }}>
+    <Draggable
+      defaultPosition={{ x: 0, y: 0 }}
+      onStart={() => {
+        setIsDragged(true);
+      }}
+      onStop={() => {
+        setIsDragged(false);
+      }}
+    >
       <div
         style={{
           cursor: "grab",
         }}
       >
-        <Card className={noteStyle}>Sample Note</Card>
+        <Card className={` ${isDragged ? animation : noteStyle}`}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis
+          odio perferendis harum dolor eligendi quaerat temporibus impedit error
+          cum magnam.
+        </Card>
       </div>
     </Draggable>
   );
